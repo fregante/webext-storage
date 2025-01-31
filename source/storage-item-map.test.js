8907efc4ae3ce6a4f655bc54a4c79c012363e474
Federico <me@fregante.com>
2025-01-31 20:52:13 +0800

    Add `StorageItemMap`

---
+++ source/storage-item-map.test.js
@@ -0,0 +1,92 @@
+import {
+	test, beforeEach, assert, expect, vi,
+} from 'vitest';
+import {StorageItemMap} from './storage-item-map.ts';
+
+const testItem = new StorageItemMap('height');
+
+function createStorage(wholeCache, area = 'local') {
+	for (const [key, data] of Object.entries(wholeCache)) {
+		chrome.storage[area].get
+			.withArgs(key)
+			.yields({[key]: data});
+	}
+}
+
+beforeEach(() => {
+	chrome.flush();
+	chrome.storage.local.get.yields({});
+	chrome.storage.local.set.yields(undefined);
+	chrome.storage.local.remove.yields(undefined);
+	chrome.storage.sync.get.yields({});
+});
+
+test('get() with empty storage', async () => {
+	assert.equal(await testItem.get('rico'), undefined);
+});
+
+test('get() with storage', async () => {
+	createStorage({
+		'height:::rico': 220,
+	});
+	assert.equal(await testItem.get('rico'), 220);
+	assert.equal(await (0, testItem.get)('rico'), 220, 'get method should be bound');
+});
+
+test('get() with default', async () => {
+	const testItem = new StorageItemMap('sign', {defaultValue: 'unknown'});
+	assert.equal(await testItem.get('december'), 'unknown');
+	createStorage({
+		'sign:::december': 'sagittarius',
+	});
+	assert.equal(await testItem.get('december'), 'sagittarius');
+});
+
+test('get() with `sync` storage', async () => {
+	const sync = new StorageItemMap('brands', {area: 'sync'});
+	await sync.get('MacBook');
+
+	assert.equal(chrome.storage.local.get.lastCall, undefined);
+
+	const argument = chrome.storage.sync.get.lastCall.args[0];
+	assert.deepEqual(argument, 'brands:::MacBook');
+});
+
+test('set() without a value matches the standard behavior (no change made)', async () => {
+	createStorage({
+		'height:::rico': 220,
+	});
+	assert.equal(await testItem.set('rico'), undefined);
+	const argument = chrome.storage.local.set.lastCall.args[0];
+	assert.deepEqual(Object.keys(argument), ['height:::rico']);
+	assert.equal(argument.name, undefined);
+});
+
+test('set() with value', async () => {
+	await testItem.set('rico', 250);
+	const argument1 = chrome.storage.local.set.lastCall.args[0];
+	assert.deepEqual(Object.keys(argument1), ['height:::rico']);
+	assert.equal(argument1['height:::rico'], 250);
+
+	await (0, testItem.set)('luigi', 120);
+	const argument2 = chrome.storage.local.set.lastCall.args[0];
+	assert.equal(argument2['height:::luigi'], 120, 'get method should be bound');
+});
+
+test('remove()', async () => {
+	await testItem.remove('mario');
+	const argument = chrome.storage.local.remove.lastCall.args[0];
+	assert.equal(argument, 'height:::mario');
+});
+
+test('onChanged() is called for the correct item', async () => {
+	const name = new StorageItemMap('distance');
+	const spy = vi.fn();
+	name.onChanged(spy);
+	chrome.storage.onChanged.trigger({unrelatedKey: 123}, 'local');
+	expect(spy).not.toHaveBeenCalled();
+	chrome.storage.onChanged.trigger({'distance:::jupiter': 10e10}, 'sync');
+	expect(spy).not.toHaveBeenCalled();
+	chrome.storage.onChanged.trigger({'distance:::jupiter': 10e10}, 'local');
+	expect(spy).toHaveBeenCalled();
+});
---
+++ source/storage-item-map.ts
@@ -0,0 +1,99 @@
+import chromeP from 'webext-polyfill-kinda';
+
+export type StorageItemMapOptions<T> = {
+	area?: chrome.storage.AreaName;
+	defaultValue?: T;
+};
+
+export class StorageItemMap<
+	/** Only specify this if you don't have a default value */
+	Base,
+
+	/** The return type will be undefined unless you provide a default value */
+	Return = Base | undefined,
+> {
+	readonly prefix: `${string}:::`;
+	readonly areaName: chrome.storage.AreaName;
+	readonly defaultValue?: Return;
+
+	constructor(
+		key: string,
+		{
+			area = 'local',
+			defaultValue,
+		}: StorageItemMapOptions<Exclude<Return, undefined>> = {},
+	) {
+		this.prefix = `${key}:::`;
+		this.areaName = area;
+		this.defaultValue = defaultValue;
+	}
+
+	has = async (secondaryKey: string): Promise<boolean> => {
+		const rawStorageKey = this.getRawStorageKey(secondaryKey);
+		const result = await chromeP.storage[this.areaName].get(rawStorageKey);
+		return Object.hasOwn(result, secondaryKey);
+	};
+
+	delete = async (secondaryKey: string): Promise<void> => {
+		const rawStorageKey = this.getRawStorageKey(secondaryKey);
+		await chromeP.storage[this.areaName].remove(rawStorageKey);
+	};
+
+	get = async (secondaryKey: string): Promise<Return> => {
+		const rawStorageKey = this.getRawStorageKey(secondaryKey);
+		const result = await chromeP.storage[this.areaName].get(rawStorageKey);
+		if (!Object.hasOwn(result, rawStorageKey)) {
+			return this.defaultValue as Return;
+		}
+
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-return -- Assumes the user never uses the Storage API directly for this key
+		return result[rawStorageKey];
+	};
+
+	set = async (secondaryKey: string, value: Exclude<Return, undefined>): Promise<void> => {
+		const rawStorageKey = this.getRawStorageKey(secondaryKey);
+		await chromeP.storage[this.areaName].set({[rawStorageKey]: value});
+	};
+
+	remove = async (secondaryKey: string): Promise<void> => {
+		const rawStorageKey = this.getRawStorageKey(secondaryKey);
+		await chromeP.storage[this.areaName].remove(rawStorageKey);
+	};
+
+	onChanged(
+		callback: (key: string, value: Exclude<Return, undefined>) => void,
+		signal?: AbortSignal,
+	): void {
+		const changeHandler = (
+			changes: Record<string, chrome.storage.StorageChange>,
+			area: chrome.storage.AreaName,
+		) => {
+			if (area !== this.areaName) {
+				return;
+			}
+
+			for (const rawKey of Object.keys(changes)) {
+				const secondaryKey = this.getSecondaryStorageKey(rawKey);
+				if (secondaryKey) {
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-argument -- Assumes the user never uses the Storage API directly
+					callback(secondaryKey, changes[rawKey]!.newValue);
+				}
+			}
+		};
+
+		chrome.storage.onChanged.addListener(changeHandler);
+		signal?.addEventListener('abort', () => {
+			chrome.storage.onChanged.removeListener(changeHandler);
+		}, {
+			once: true,
+		});
+	}
+
+	private getRawStorageKey(secondaryKey: string): string {
+		return this.prefix + secondaryKey;
+	}
+
+	private getSecondaryStorageKey(rawKey: string): string | false {
+		return rawKey.startsWith(this.prefix) && rawKey.slice(this.prefix.length);
+	}
+}
